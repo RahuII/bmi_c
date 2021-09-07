31213c2ec6a683744057975b3f60f85579832068
rahul138 <rahul.kumar17407@marwadieducation.edu.in>
2021-09-07 17:02:15 +0530

    Remove extract widget BottomButton

--- lib/inputPage.dart
+++ lib/inputPage.dart
@@ -241,31 +241,6 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
-// class BottomButton extends StatelessWidget {
-//   BottomButton({required this.onTap, required this.buttonTitle})
-//   final Function() onTap;
-//   final String buttonTitle;
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return GestureDetector(
-//       onTap: onTap,
-//       child: Container(
-//         color: kbottomCardColour,
-//         margin: EdgeInsets.only(top: 10),
-//         height: kbottomContainerHeight,
-//         width: double.infinity,
-//         child: Center(
-//           child: Text(
-//             buttonTitle,
-//             style: kLargeButtonTextStyle,
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 class RoundIconButton extends StatelessWidget {
   RoundIconButton({required this.icon, required this.onPressed});
   final IconData icon;
